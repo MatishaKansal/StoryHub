dcd777ff3bf3b4b7a9b0c4d5fe19c67bfea2812d
MatishaKansal <64193940+MatishaKansal@users.noreply.github.com>
2020-12-01 20:39:53 +0530

    Add files via upload

---
+++ screens/WriteStoryScreen.js
@@ -0,0 +1,82 @@
+import * as React from "react";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+import { Header } from "react-native-elements";
+import * as firebase from "firebase";
+import db from "../config";
+
+export default class WriteStoryScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      author: "",
+      title: "",
+      story: "",
+    };
+  }
+  submit = () => {
+    db.collection("Book").add({
+      Author: this.state.author,
+      Title: this.state.title,
+      Story: this.state.story,
+    });
+  };
+
+  render() {
+    return (
+      <View>
+        <Header
+          backgroundColor={"#F2CEDF"}
+          centerComponent={{
+            text: "STORY HUB",
+            style: { fontSize: 24 },
+          }}
+        ></Header>
+        <TextInput
+          style={styles.titleInput}
+          placeholder="Title of the story"
+          value={this.state.title}
+        />
+        <TextInput
+          style={styles.titleInput}
+          placeholder="Author of the story"
+          value={this.state.author}
+        />
+        <TextInput
+          style={styles.contentInput}
+          placeholder="Content of the story"
+          value={this.state.story}
+        />
+        <TouchableOpacity
+          style={{ backgroundColor: "#F2CEDF", margin: 150, width: 100 }}
+          onPress={() => {
+            this.submit();
+          }}
+        >
+          <Text style={{ fontSize: 24, textAlign: "center" }}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  titleInput: {
+    borderWidth: 1.5,
+    width: 380,
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  contentInput: {
+    borderWidth: 1.5,
+    width: 380,
+    marginLeft: 10,
+    marginTop: 10,
+    height: 200,
+  },
+});
